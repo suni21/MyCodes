fc7b2930b74c29fdf5740e1833bd53e3e176fec3
suni21 <chaudharysuniti21gmail.com>
2021-08-30 21:03:25 +0530

    new

---
+++ OOPs/24Aug21/Q7Book.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Books
+{
+    char author[30];
+    char title[30];
+    char pub[50];
+    float price;
+    int copies;
+
+    public:
+    Books();
+};
