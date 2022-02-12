e33911b247fcaddfb688204ea7e85e9c8daeef04
suni21 <83060711+suni21@users.noreply.github.com>
2022-02-12 22:24:18 +0530

    new

--- OS/q2SJF_NonPre.c
+++ OS/q2SJF_NonPre.c
@@ -1,3 +1,7 @@
+/* Name:Suniti
+Rollno:2006243
+*/ 
+
 #include <stdio.h>
 
 int main()
@@ -7,8 +11,8 @@ int main()
     int pr, add = 0;
     int total_TAT = 0, total_WT = 0;
     float avg_TAT, avg_WT;
-
-    int i, j;
+    int btime=0;
+    int i, j, k=1,min;
 
     printf("Enter no. of processes: ");
     scanf("%d", &pr);
@@ -32,27 +36,50 @@ int main()
 
     printf("\n");
 
-    // sorting arival time and busttime
     for (i = 0; i < pr; i++)
-    {
-        for (j = 0; j < pr; j++)
-        {
-            if (at[i] < at[j])
-            {
-                int temp = p[j];
-                p[j] = p[i];
-                p[i] = temp;
+	{
+		for (j = 0; j < pr; j++)
+		{
+			if (at[i] < at[j])
+			{
+				int temp = p[j];
+				p[j] = p[i];
+				p[i] = temp;
 
-                temp = at[j];
-                at[j] = at[i];
-                at[i] = temp;
+				temp = at[j];
+				at[j] = at[i];
+				at[i] = temp;
 
-                temp = bt[j];
-                bt[j] = bt[i];
-                bt[i] = temp;
-            }
-        }
-    }
+				temp = bt[j];
+				bt[j] = bt[i];
+				bt[i] = temp;
+			}
+		}
+	}
+	for (j = 0; j < pr; j++)
+	{
+        
+		btime = btime + bt[j];
+		min = bt[k];
+		for (i = k; i < pr; i++)
+		{
+			if (btime >= at[i] && bt[i] < min)
+             {
+				int temp = p[k];
+				p[k] = p[i];
+				p[i] = temp;
+
+				temp = at[k];
+				at[k] = at[i];
+				at[i] = temp;
+
+				temp = bt[k];
+				bt[k] = bt[i];
+				bt[i] = temp;
+			}
+		} k++;
+	}
+
 
     // calculate completion time (CT)
     for (j = 0; j < pr; j++)
@@ -64,16 +91,10 @@ int main()
     wt[0] = 0;
     for (i = 1; i < pr; i++)
     {
-        wt[i] = 0;
-        for (j = 0; j < i; j++)
-            wt[i] += bt[j];
+        tat[i]=ct[i]-at[i];
+        wt[i]=tat[i]-bt[i];
     }
-    // calculate turn around time (TAT)
-    for (j = 0; j < pr; j++)
-    {
-        tat[j] = bt[j] + wt[j];
-    }
-
+  
     for (i = 0; i < pr; i++)
     {
         total_WT = total_WT + wt[i];
@@ -91,28 +112,6 @@ int main()
     avg_WT = total_WT / pr;
     printf("\n\nAverage Turnaround Time= %.3f\n", avg_TAT);
     printf("Average Waiting time= %.3f\n\n", avg_WT);
-    printf("----------------------------------------------\n");
-    for ( i=0; i<pr; i++)
-        {
-            printf("|  P%d  ",p[i]);
-            printf("  ");
-
-        }
-        printf("|");
-
-    printf("\n");
-    printf("----------------------------------------------\n");
-    printf("0");
-    for ( i=0; i<pr; i++)
-        {
-             for ( j=0; j<bt[i]; j++)
-                 {
-                     printf("  ");
-                 }
-              if(tat[i]>9)
-                  printf("  ");
-
-        printf("%d", tat[i]);
-        }
+  
     return 0;
 }
