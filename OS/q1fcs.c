91c93267192910b1f5c6e18a1086f561c13433aa
suni21 <83060711+suni21@users.noreply.github.com>
2022-02-14 11:37:39 +0530

    new

--- OS/q1fcs.c
+++ OS/q1fcs.c
@@ -1,92 +1,88 @@
 /* Name:Suniti
 Rollno:2006243
-*/ 
+*/
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
 
-	int p[5]={0}, bt[5]={0}, at[5]={0}, tat[5]={0}, wt[5]={0}, ct[5]={0};
-	int pr, add=0;
-	int total_TAT=0, total_WT=0;
+    int p[5] = {0}, bt[5] = {0}, at[5] = {0}, tat[5] = {0}, wt[5] = {0}, ct[5] = {0};
+    int pr, add = 0, total_TAT = 0, total_WT = 0;
     float avg_TAT, avg_WT;
-    int i,j;
+    int i, j;
 
-	printf("Enter no. of processes: ");
-	scanf("%d",&pr);
+    printf("Enter no. of processes: ");
+    scanf("%d", &pr);
 
-//Arrival time (AT) and Bust time(BT)
-    printf("Enter Arrival time= \n");
-    for(i=0;i<pr;i++)
+    // Arrival time (AT) and Bust time(BT)
+    printf("Enter Arrivaltime= \n");
+    for (i = 0; i < pr; i++)
     {
-        printf("p[%d]= ", i+1);
-		scanf("%d", &at[i]);
-        p[i]=i+1;
+        printf("p[%d]= ", i + 1);
+        scanf("%d", &at[i]);
+        p[i] = i + 1;
     }
 
-    printf("Enter Bust time= \n");
-	for(i=0;i<pr;i++)
+    printf("Enter Busttime= \n");
+    for (i = 0; i < pr; i++)
     {
-		printf("p[%d]= ", i+1);
-		scanf("%d", &bt[i]);
-        p[i]=i+1;
-	}
-  
-    	printf("\n");	
-         
-// sorting arival time and busttime
-  for (i = 0; i < pr - 1; i++)
+        printf("p[%d]= ", i + 1);
+        scanf("%d", &bt[i]);
+        p[i] = i + 1;
+    }
+
+    printf("\n");
+
+    // sorting arival time and busttime
+    for (i = 0; i < pr - 1; i++)
     {
-        for (j = 0 ; j < pr - i - 1 ; j++)
+        for (j = 0; j < pr - i - 1; j++)
         {
-            if(at[j]>at[j+1])
-              {
-                int temp=p[j];
-                p[j]=p[j+1];
-                p[j+1]=temp;
+            if (at[j] > at[j + 1])
+            {
+                int temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
 
-                temp=at[j];
-                at[j]=at[j+1];
-                at[j+1]=temp;
+                temp = at[j];
+                at[j] = at[j + 1];
+                at[j + 1] = temp;
 
-                temp=bt[j];
-                bt[j]=bt[j+1];
-                bt[j+1]=temp;
-
-              }
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+            }
         }
-            
-    }  
+    }
 
-//calculate completion time of processes 	
-	for(j=0;j<pr;j++){
-		add+=bt[j];
-		ct[j]+=add;
-	}
-//Calculate watingtime, trun around time
-wt[0]=0;	
-for(j=0;j<pr;j++)
-{
-      tat[j]=ct[j]-at[j];
-      wt[j]=tat[j]-bt[j];
-	}
-//Toatl calculation
-for ( i=0; i<pr; i++) 
-{
-      total_WT = total_WT + wt[i];
-      total_TAT = total_TAT + tat[i];
-}
-	printf("PID\t AT\t BT\t CT\t TAT\t WT\t\n");
-	for(i=0;i<pr;i++)
+    for (j = 0; j < pr; j++) // CompletionTime (CT)
     {
-		printf("P%d\t %d\t %d\t %d\t %d\t %d\n",p[i],at[i],bt[i],ct[i],tat[i],wt[i]);
-	}
+        add += bt[j];
+        ct[j] += add;
+    }
+    // Calculate watingtime, trun around time
+    wt[0] = 0;
+    for (j = 0; j < pr; j++)
+    {
+        tat[j] = ct[j] - at[j];
+        wt[j] = tat[j] - bt[j];
+    }
+    for (i = 0; i < pr; i++) // Toatl calculation
+    {
+        total_WT = total_WT + wt[i];
+        total_TAT = total_TAT + tat[i];
+    }
+    printf("PID\t AT\t BT\t CT\t TAT\t WT\t\n");
+    for (i = 0; i < pr; i++)
+    {
+        printf("P%d\t %d\t %d\t %d\t %d\t %d\n", p[i], at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
 
-	avg_TAT=total_TAT/pr;
-    avg_WT=total_WT/pr;
-	printf("\n\nAverage Turnaround Time= %f\n",avg_TAT);
-	printf("Average Waiting time= %f\n\n",avg_WT);
-	    
-	return 0;
+    avg_TAT = total_TAT / pr;
+    avg_WT = total_WT / pr;
+    printf("\n\nAverage Turnaround Time= %f\n", avg_TAT);
+    printf("Average Waiting time= %f\n\n", avg_WT);
+
+    return 0;
 }
