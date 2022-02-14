485f4c01560dedf455c22e3b24e4368d4b85704b
suni21 <83060711+suni21@users.noreply.github.com>
2022-02-14 11:37:32 +0530

    new

--- OS/q2SJF_NonPre.c
+++ OS/q2SJF_NonPre.c
@@ -8,25 +8,22 @@ int main()
 {
 
     int p[5] = {0}, bt[5] = {0}, at[5] = {0}, tat[5] = {0}, wt[5] = {0}, ct[5] = {0};
-    int pr, add = 0;
-    int total_TAT = 0, total_WT = 0;
+    int add = 0, total_TAT = 0, total_WT = 0, btime=0, m=1;
     float avg_TAT, avg_WT;
-    int btime=0;
-    int i, j, k=1,min;
+    int pr, i, j,min;
 
     printf("Enter no. of processes: ");
     scanf("%d", &pr);
 
-    // Arrival time (AT) 
-    printf("\nEnter Arrival time= \n");
+    printf("\nEnter Arrivaltime= \n");    // Arrival time (AT)
     for (i = 0; i < pr; i++)
     {
         printf("P[%d]= ", i + 1);
         scanf("%d", &at[i]);
         p[i] = i + 1;
     }
-    // Bust time(BT)
-    printf("\nEnter Bust time= \n");
+
+    printf("\nEnter Busttime= \n");    // Bust time(BT)
     for (i = 0; i < pr; i++)
     {
         printf("P[%d]= ", i + 1);
@@ -35,8 +32,7 @@ int main()
     }
 
     printf("\n");
-
-    for (i = 0; i < pr; i++)
+    for (i = 0; i < pr; i++)  //Sorting as per arrivaltime
 	{
 		for (j = 0; j < pr; j++)
 		{
@@ -56,46 +52,42 @@ int main()
 			}
 		}
 	}
-	for (j = 0; j < pr; j++)
+	for (j = 0; j < pr; j++)  //Sorting as per busttime
 	{
         
 		btime = btime + bt[j];
-		min = bt[k];
-		for (i = k; i < pr; i++)
+		min = bt[m];
+		for (i = m; i < pr; i++)
 		{
 			if (btime >= at[i] && bt[i] < min)
              {
-				int temp = p[k];
-				p[k] = p[i];
+				int temp = p[m];
+				p[m] = p[i];
 				p[i] = temp;
 
-				temp = at[k];
-				at[k] = at[i];
+				temp = at[m];
+				at[m] = at[i];
 				at[i] = temp;
 
-				temp = bt[k];
-				bt[k] = bt[i];
+				temp = bt[m];
+				bt[m] = bt[i];
 				bt[i] = temp;
 			}
-		} k++;
+		} m++;
 	}
 
-
-    // calculate completion time (CT)
-    for (j = 0; j < pr; j++)
+    for (j = 0; j < pr; j++)   //CompletionTime (CT)
     {
         add += bt[j];
         ct[j] += add;
     }
-    // calculate wating time (WT)
     wt[0] = 0;
-    for (i = 1; i < pr; i++)
+    for (i = 1; i < pr; i++)  // Watingtime (WT)
     {
         tat[i]=ct[i]-at[i];
         wt[i]=tat[i]-bt[i];
     }
-  
-    for (i = 0; i < pr; i++)
+    for (i = 0; i < pr; i++)   //Total
     {
         total_WT = total_WT + wt[i];
         total_TAT = total_TAT + tat[i];
@@ -107,7 +99,6 @@ int main()
     {
         printf("P%d\t %d\t %d\t %d\t %d\t %d\n", p[i], at[i], bt[i], ct[i], tat[i], wt[i]);
     }
-
     avg_TAT = total_TAT / pr;
     avg_WT = total_WT / pr;
     printf("\n\nAverage Turnaround Time= %.3f\n", avg_TAT);
