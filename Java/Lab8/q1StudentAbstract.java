de644fb00b1255a3b3e5216e97c0be25cf2ea33c
suni21 <83060711+suni21@users.noreply.github.com>
2022-04-17 22:50:49 +0530

    new

---
+++ Java/Lab8/q1StudentAbstract.java
@@ -0,0 +1,36 @@
+package Lab8;
+import java.util.Scanner;
+
+class q1StudentAbstract 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        int r, re;
+        System.out.println("Enter rollno.= ");
+        r = s.nextInt();
+        System.out.println("Enter registration no.= ");
+        re = s.nextInt();
+        kiitian k = new kiitian();
+        k.course(r, re);
+        s.close();
+    }
+}
+
+abstract class student  //abstract class
+{
+    public int roll, reg;
+    abstract void course(int r, int re); //abstract method
+}
+
+class kiitian extends student 
+{
+    void course(int r, int re) 
+    {
+        roll = r;
+        reg = re;
+        System.out.println("\nDetalis: ");
+        System.out.println("Roll no.= " + r);
+        System.out.println("Registration no.= " + re);
+    }
+}
