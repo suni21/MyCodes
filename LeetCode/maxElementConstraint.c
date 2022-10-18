6578ee70e1b13c4a981fc34371660c9fef62f34c
suni21 <83060711+suni21@users.noreply.github.com>
2022-10-18 23:43:53 +0530

    new

---
+++ LeetCode/maxElementConstraint.c
@@ -0,0 +1,20 @@
+int maxValue(int n, int k, int maxSum)
+{
+    if (n == 1)
+        return maxSum;
+    int right = k, left = k;
+    int count = 1;
+    int limitRight = n - 1;
+    while (n <= maxSum && (left > 0 || right < limitRight))
+    {
+        n += right - left + 1;
+        if (left > 0)
+            left--;
+        if (right < limitRight)
+            right++;
+        count++;
+    }
+    if (n < maxSum)
+        count += (maxSum - n) / (right - left + 1) + 1;
+    return count - 1;
+}
